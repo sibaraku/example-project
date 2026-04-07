22853f0f8be35fac6643debcf2a48c6a83d060ce
sibaraku <189846437+sibaraku@users.noreply.github.com>
2026-04-07 08:39:51 +0300

    Andmebaas ja JSON import

--- backend/models/energyreading.js
+++ backend/models/energyreading.js
@@ -16,6 +16,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       sequelize,
       modelName: "EnergyReading",
+      timestamps: true,
       tableName: "EnergyReadings",
       underscored: true,
     },
